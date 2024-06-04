379c53eae093236cfd4f550f6c96b1c44a2cfd9c
MariaERodriguez <mariaeugeniarodriguez2002@gmail.com>
2024-06-04 02:03:53 -0300

    Menu principal
    Problemas con el reconocimiento de un botón.

--- Assets/Scripts/Main_Menu.cs
+++ Assets/Scripts/Main_Menu.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Main_Menu : MonoBehaviour
 {
+   public Button primaryButton;
+
+   void Start()
+    {
+        primaryButton.Select();
+    }
+
    public void Level_Start(string level)
     {
         SceneManager.LoadScene(level);
