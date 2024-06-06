ca56346c5994360d31dbeaceae22d3acc341b7f0
Mfischer <57914018+Mfosh@users.noreply.github.com>
2024-06-05 21:50:43 -0300

    prefabs

--- Assets/Scripts/BallPool.cs
+++ Assets/Scripts/BallPool.cs
@@ -8,7 +8,7 @@ public class BallPool : MonoBehaviour
     public GameObject BallPrefab;
     public int MaxBalls;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         ballsPool = new Queue<GameObject>();
 
