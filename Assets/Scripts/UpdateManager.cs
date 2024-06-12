e859a4f2e03e5b62c682e42126072e555e7c6a46
MariaERodriguez <mariaeugeniarodriguez2002@gmail.com>
2024-06-12 11:01:43 -0300

    L

--- Assets/Scripts/UpdateManager.cs
+++ Assets/Scripts/UpdateManager.cs
@@ -7,11 +7,6 @@ public class UpdateManager : MonoBehaviour
 
     [SerializeField] public List <IUpdateable> updateables = new List<IUpdateable>();
     [SerializeField] int a;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
